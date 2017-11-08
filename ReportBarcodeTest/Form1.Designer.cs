90b719f2b23121719d21c880b378a3f06dd370d9
Pc <Pc@TIS_SUSIC>
2017-11-08 14:15:10 +0300

    Add project files.

---
+++ ReportBarcodeTest/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace ReportBarcodeTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.dskagit = new ReportBarcodeTest.dskagit();
+            this.kagitBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.kagitTableAdapter = new ReportBarcodeTest.dskagitTableAdapters.kagitTableAdapter();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dskagit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kagitBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            reportDataSource1.Name = "dskagit";
+            reportDataSource1.Value = this.kagitBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "ReportBarcodeTest.Report1.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(33, 71);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.Size = new System.Drawing.Size(436, 161);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // dskagit
+            // 
+            this.dskagit.DataSetName = "dskagit";
+            this.dskagit.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // kagitBindingSource
+            // 
+            this.kagitBindingSource.DataMember = "kagit";
+            this.kagitBindingSource.DataSource = this.dskagit;
+            // 
+            // kagitTableAdapter
+            // 
+            this.kagitTableAdapter.ClearBeforeFill = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "65258";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(149, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(899, 638);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dskagit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kagitBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource kagitBindingSource;
+        private dskagit dskagit;
+        private dskagitTableAdapters.kagitTableAdapter kagitTableAdapter;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ReportBarcodeTest/Form1.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReportBarcodeTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dskagit.kagit' table. You can move, or remove it, as needed.
+            this.kagitTableAdapter.Fill(this.dskagit.kagit);
+
+            this.reportViewer1.RefreshReport();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            ReportParameter Barkod = new ReportParameter("Barkod", textBox1.Text);
+            this.reportViewer1.LocalReport.SetParameters(new ReportParameter[] { Barkod });
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
